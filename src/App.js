e844d4ba1c74558a3c586de0fc18e90ba07dd6be
kidde60 <kgeorgewilliam60@gmail.com>
2026-05-31 10:53:43 +0300

    Update build

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { useState, useRef } from "react";
-import { X, FileText } from "lucide-react";
 import InvoiceForm from "./components/InvoiceForm";
 import InvoicePreview from "./components/InvoicePreview";
 import { downloadPDF } from "./utils/pdfGenerator";
